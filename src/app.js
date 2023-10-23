4b7c16c88e9c10d5b9af2798446b7264b4976ba2
Moises1235 <127400299+Moises1235@users.noreply.github.com>
2023-10-23 10:09:33 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import Express from 'express';
 import logger from 'morgan';
-import rotasDeUsuarios from './rotas/rotasDeUsuarios.js';
+import rotasDePassageiros from './rotas/rotasDePassageiros.js';
+import rotasDeMotoristas from './rotas/rotasDeMotoristas.js';
+import rotasDeViagens from './rotas/rotasDeViagens.js';
+import rotasDeCompras from './rotas/rotasDeCompras.js';
+import rotasDeConversas from './rotas/rotasDeConversas.js';
+import rotasDePagamentos from './rotas/rotasDePagamentos.js';
+import rotasDeAgendamentos from './rotas/rotasDeAgendamentos.js';
 
 const app = Express();
 app.use(logger('dev'));
 
-app.use('/usuarios', rotasDeUsuarios);
+app.use('/passageiros', rotasDePassageiros);
+app.use('/motoristas', rotasDeMotoristas);
+app.use('/viagens', rotasDeViagens);
+app.use('/compras', rotasDeCompras);
+app.use('/conversas', rotasDeConversas);
+app.use('/pagamentos', rotasDePagamentos);
+app.use('/agendamentos', rotasDeAgendamentos);
+
+
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
